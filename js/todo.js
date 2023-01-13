0edf216363384bef05a7325b05979d37d89be8b0
greyhawk16 <hitec16@naver.com>
2023-01-13 09:03:10 +0900

    Geolocate, Weather API created
    #7.6 ~ #8.2
    Geolocating funtion, Weather API created

--- js/todo.js
+++ js/todo.js
@@ -13,13 +13,16 @@ function saveToDos() {
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
+    saveToDos();
 }
 
 
 function paintToDo(newToDo) {
     const li = document.createElement("li");
+    li.id = newToDo.id;
     const span = document.createElement("span");
-    span.innerText = newToDo;
+    span.innerText = newToDo.text;
 
     const button = document.createElement("button");
     button.innerText = "❌";
@@ -35,19 +38,24 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
+
+    const newTodoObj = {
+        text: newToDo,
+        id: Date.now(),
+    };
+
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if (saveToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
